888a3570c354a60b99f99543c12d8e29f9a340a9
Junior Nascimento <65140975+JuniorNascimento777@users.noreply.github.com>
2024-03-24 23:36:32 -0300

    Update ComponenteFilho.jsx

--- atividade 02/02Pokémon/ComponenteFilho.jsx
+++ atividade 02/02Pokémon/ComponenteFilho.jsx
@@ -3,18 +3,18 @@ import ComponenteNeto from "./ComponenteNeto";
 import { ContextoInteiro } from "./ContextoInteiro";
 
 const ComponenteFilho = () => {
-  const { inteiro, nextPokemon } = useContext(ContextoInteiro);
-
-  return (
-    <div>
-      Filho
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro + 1}.png`}
-      />
-      <button onClick={() => nextPokemon()}>Próximo</button>
-      <ComponenteNeto />
-    </div>
-  );
+    const { inteiro, nextPokemon } = useContext(ContextoInteiro);
+    return (
+        <div>
+            Filho
+            <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro + 1}.png`}
+            />
+            <button onClick={() => nextPokemon()}>Próximo</button>
+            <ComponenteNeto />
+        </div>
+    );
 };
+
 
 export default ComponenteFilho;
