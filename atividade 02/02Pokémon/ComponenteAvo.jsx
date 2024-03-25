b9fce600915d84f4daff08c4f47fea2c6a8b1571
Junior Nascimento <65140975+JuniorNascimento777@users.noreply.github.com>
2024-03-24 23:34:50 -0300

    Update ComponenteAvo.jsx

--- atividade 02/02Pokémon/ComponenteAvo.jsx
+++ atividade 02/02Pokémon/ComponenteAvo.jsx
@@ -1,23 +1,21 @@
-import React, { createContext, useState } from 'react'
-import ComponenteFilho from './ComponenteFilho'
+import React, { createContext, useState } from 'react';
+import ComponenteFilho from './ComponenteFilho';
 import { ContextoInteiro } from './ContextoInteiro';
 
 const ComponenteAvo = () => {
-  const [inteiro, setInteiro] = useState(1);
+    const [inteiro, setInteiro] = useState(1);
 
-  function nextPokemon() {
-    setInteiro((i) => i + 1);
-  }
-
-  return (
-    <div style={{ width: "40rem", display: "flex" }}>
-      <ContextoInteiro.Provider value={{ inteiro, nextPokemon }}>
-        Avô
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro}.png`} />
-        <ComponenteFilho />
-      </ContextoInteiro.Provider>
-    </div>
-  )
-}
-
-export default ComponenteAvo
+    function nextPokemon() {
+        setInteiro((i) => i + 1);
+    }
+    return (
+        <div style={{ width: "40rem", display: "flex" }}>
+            <ContextoInteiro.Provider value={{ inteiro, nextPokemon }}>
+                Avô
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro}.png`} />
+                <ComponenteFilho />
+            </ContextoInteiro.Provider>
+        </div>
+    );
+};
+export default ComponenteAvo;
