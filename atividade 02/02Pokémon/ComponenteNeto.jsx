946805f815146d72e0f5361ac463e673cb83e221
Junior Nascimento <65140975+JuniorNascimento777@users.noreply.github.com>
2024-03-24 23:37:23 -0300

    Update ComponenteNeto.jsx

--- atividade 02/02Pokémon/ComponenteNeto.jsx
+++ atividade 02/02Pokémon/ComponenteNeto.jsx
@@ -1,18 +1,16 @@
 import React, { useContext } from "react";
 import { ContextoInteiro } from "./ContextoInteiro";
-
 const ComponenteNeto = () => {
+   
   const { inteiro, nextPokemon } = useContext(ContextoInteiro);
-
-  return (
-    <div>
-      Neto
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro + 2}.png`}
-      />
-      <button onClick={() => nextPokemon()}>Próximo</button>
-    </div>
-  );
+    return (
+        <div>
+            Neto
+            <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${inteiro + 2}.png`}
+            />
+            <button onClick={() => nextPokemon()}>Próximo</button>
+        </div>
+    );
 };
-
 export default ComponenteNeto;
