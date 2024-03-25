e247df535bafdd08720c59b9fe050fa469e8db1f
Junior Nascimento <65140975+JuniorNascimento777@users.noreply.github.com>
2024-03-24 23:43:02 -0300

    Update FunctionContextC.jsx

--- atividade 02/FunctionContextC.jsx
+++ atividade 02/FunctionContextC.jsx
@@ -1,22 +1,19 @@
 import ColorTheme from "./MyColorContext";
 import FunctionContextD from "./FunctionContextD";
 import { useContext } from "react";
-
 const FunctionContextC = () => {
 
     const { bkgC } = useContext(ColorTheme);
-
-  return (
-    <ColorTheme.Consumer>
-      {(value) => {
-        return (
-          <div>
-            <h1 style={{ backgroundColor: bkgC }}>Contexto C</h1>
-          </div>
-        );
-      }}
-    </ColorTheme.Consumer>
-  );
+ return (
+        <ColorTheme.Consumer>
+            {(value) => {
+                return (
+                    <div>
+                        <h1 style={{ backgroundColor: bkgC }}>Contexto C</h1>
+                    </div>
+                );
+            }}
+        </ColorTheme.Consumer>
+    );
 };
-
 export default FunctionContextC;
